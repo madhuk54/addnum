de83daa6b51d98b2fd2d87e0168f1e68b4e14c0f
madhuk54 <krishnashetty98765@gmail.com>
2025-03-19 11:14:39 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,28 +1,22 @@
+package addnum;
 import java.util.Scanner;
+
 
 public class add {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num1 = 0, num2 = 0;
-
-        System.out.println("Enter first number: ");
-        if (sc.hasNextInt()) {
-            num1 = sc.nextInt();
-        } else {
-            System.out.println("No input provided, using default value 10.");
-            num1 = 10;
-        }
-
-        System.out.println("Enter second number: ");
-        if (sc.hasNextInt()) {
-            num2 = sc.nextInt();
-        } else {
-            System.out.println("No input provided, using default value 20.");
-            num2 = 20;
-        }
-
-        System.out.println("Sum: " + (num1 + num2));
-        sc.close();
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        
+        int sum = num1 + num2;
+        
+        System.out.println("The sum is: " + sum);
+        
+        scanner.close();
     }
 }
 
