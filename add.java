32c0604ce24698a7e0d096f7157ddb4d7f5a230a
madhuk54 <krishnashetty98765@gmail.com>
2025-03-19 11:22:38 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,21 +1,16 @@
-import java.util.Scanner;
-
-
-public class add {
+public class Add {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter first number: ");
-        int num1 = scanner.nextInt();
-        
-        System.out.print("Enter second number: ");
-        int num2 = scanner.nextInt();
-        
+        if (args.length < 2) {
+            System.out.println("Usage: java Add <num1> <num2>");
+            return;
+        }
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
         int sum = num1 + num2;
-        
+
         System.out.println("The sum is: " + sum);
-        
-        scanner.close();
     }
 }
 
